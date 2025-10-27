c34ed63b34e57e5932d5e1c12ae937ca97e2963d
AllCityPages <bizdelc@gmail.com>
2025-10-26 20:54:39 -0500

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,20 @@
-// public/service-worker.js
+// Minimal offline + notification click handler
 const CACHE = 'acp-coupons-v1';
+const ASSETS = [
+  '/',
+  '/offers.html',
+  '/wallet.html'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
-    '/', '/offers.html', '/wallet.html', '/manifest.json'
-  ])));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(()=>new Response('Offline',{status:503})))
   );
+});
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/offers.html'));
 });
